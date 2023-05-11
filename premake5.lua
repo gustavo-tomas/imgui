54da1180ebb4e55f286268e427497b5b1f15615a
gustavo-tomas <guga.tomas10@gmail.com>
2023-05-11 15:16:55 -0300

    Updating premake

--- premake5.lua
+++ premake5.lua
@@ -1,40 +1,40 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+	cppdialect "C++17"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/%{cfg.buildcfg}/%{prj.name}")
+    objdir ("bin/build/%{prj.name}")
+
+	includedirs { "." }
 
 	files
 	{
-		"imconfig.h",
 		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
+		"imconfig.h",
 		"imgui_internal.h",
-		"imgui_tables.cpp",
-		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
-	}
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imgui_demo.cpp",
 
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
+		"backends/imgui_impl_glfw.cpp",
+		"backends/imgui_impl_opengl3.cpp"
+	}
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
-		cppdialect "C++17"
 		staticruntime "On"
 
-	filter "configurations:Debug"
+	filter "configurations:debug"
 		runtime "Debug"
 		symbols "on"
 
-	filter "configurations:Release"
+	filter "configurations:release"
 		runtime "Release"
 		optimize "on"
